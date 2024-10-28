c3299dc9311cb813fa1741bac88d11a46dcacfa6
Abdul Ahad <ahad83392@gmail.com>
2024-10-28 10:33:06 -0700

    Update project files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Navz from './Zameen.com/Navz';
-import Body from './Zameen.com/Body';
-import Bottom from './Zameen.com/Bottom';
-import Calculator from './Zameen.com/Calculator';
-import './Zameen.com/Zameen.css'
-import Example from './Zameen.com/Nav';
+import Menu from './Project/Menu';
+import Main from './Project/Main';
+import './Project/Project.css'
+import Portfoilo from './Project/Portfoilo';
+import About from './Project/About';
+import Contact from './Project/Contact';
+// import Navz from './Zameen.com/Navz';
+// import Body from './Zameen.com/Body';
+// import Bottom from './Zameen.com/Bottom';
+// import Calculator from './Zameen.com/Calculator';
+// import './Zameen.com/Zameen.css'
+// import Example from './Zameen.com/Nav';
 // import './Project/Project.css';
 // import Main from './Project/Main';
 // import Portfoilo from './Project/Portfoilo';
@@ -17,12 +23,18 @@ import Example from './Zameen.com/Nav';
 function App() {
   return (
     <div className="App">
+      <Menu />
+      <Main />
+      <Portfoilo />
+      <About />
+      <Contact/>
+
      
-      <Navz />
+      {/* <Navz />
       <Example/>
       <Body />
       <Bottom />
-      <Calculator/>
+      <Calculator/> */}
 
       {/* <Main />
       <Menu />
